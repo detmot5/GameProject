535e18f5381ee7945c8960df421b305bd064662c
Norbert Bielak <norbertbielak@gmail.com>
2020-04-26 11:50:16 +0200

    Rendering and updating animations in vector

--- Level.h
+++ Level.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <vector>
+
 #include "GameLevel.h"
 #include "Block.h"
 #include "World.h"
+
+#include "Animation/Animation.h"
+#include "Character.h"
+#include "Clouds.h"
 
 
 class Level : public GameLevel
@@ -13,6 +19,5 @@ public:
 	void Unload() override;
 	void Render() override;
 	void Update() override;
-
 };
 
