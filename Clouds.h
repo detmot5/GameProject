14ff2a5fc0481b7f3a087bccd87a8e34cd8e8a22
Norbert Bielak <norbertbielak@gmail.com>
2020-04-27 22:31:49 +0200

    Changed cloud speed"

--- Clouds.h
+++ Clouds.h
@@ -6,7 +6,7 @@ class Clouds : public Animation
 {
 public:
 
-	Clouds(LPCTSTR bitmapPath, Graphics* graphics, float x = SCREEN_WIDTH / SCREEN_WIDTH - 1, float y = (int)(SCREEN_HEIGHT * 0.10), float xSpeed = 2.0);
+	Clouds(LPCTSTR bitmapPath, Graphics* graphics, float x = SCREEN_WIDTH / SCREEN_WIDTH - 1, float y = (int)(SCREEN_HEIGHT * 0.10), float xSpeed = 0.02);
 	
 private:
 
