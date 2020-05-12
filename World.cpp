3494c29d1a56dff801aa28aa57588d7f14ce419e
Norbert Bielak <norbertbielak@gmail.com>
2020-05-12 22:17:10 +0200

    Added automatic chunk generator

--- World.cpp
+++ World.cpp
@@ -2,7 +2,7 @@
 using namespace Utils;
 
 
-vector<Chunk*> World::chunks;
+list<Chunk*> World::chunks;
 Chunk* World::actualChunk;
 
 
@@ -17,7 +17,7 @@ UINT16 World::GetActualFloorLevel(UINT16 x) {
 
 UINT16 World::FindNearestLand(UINT16 objPosX, UINT16 objPosY) {
 	for (UINT8 i = objPosY / 32; i < blocksCountY; i++) {
-		if (actualChunk->isCollisionEnabled(*actualChunk ,objPosX / 32, i)) return i * 32;
+		if (actualChunk->isCollisionEnabled(objPosX / 32, i)) return i * 32;
 	}
 	return averageFloorLevel;
 }
@@ -29,12 +29,33 @@ void World::Init(Graphics* gfx) {
 	Chunk::Init(gfx);
 	chunks.push_back(new Chunk(0));
 	chunks.push_back(new Chunk(25));
-	actualChunk = chunks[0];
+
+	actualChunk = chunks.front();
 }
 
 
 void World::Update() {
-	for (auto i : chunks) i->SetOffset(offset);
+	UINT16 actualStartPoint;
+	for (auto i : chunks) {
+		actualStartPoint = chunks.back()->GetStartPoint();
+		i->SetOffset(offset);
+	}
+	if (abs(offset / 25) >= actualStartPoint) {
+		actualChunk = chunks.back();
+		chunks.push_back(new Chunk(actualStartPoint + 25));
+#if DEBUG_MODE 
+		cout << "Generated" << endl; 
+		cout << (offset/25)  << endl;
+#endif
+	}
+	if (abs(offset / 25) - chunks.front()->GetStartPoint() > 200) {
+		chunks.pop_front();
+#if DEBUG_MODE
+		cout << "Deleted" << endl;
+#endif
+	}
+	cout << offset/25 << endl;
+
 }
 
 void World::Render() {
