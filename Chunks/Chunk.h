62f384727e790e4482df9497c7b8738b5c5074ed
Norbert Bielak <norbertbielak@gmail.com>
2020-05-15 15:17:21 +0200

    Maket chunk size variables const and public

--- Chunks/Chunk.h
+++ Chunks/Chunk.h
@@ -28,13 +28,11 @@ public:
 	inline void SetOffset(double offset) { this->offset = offset; };
 	inline double GetOffset() { return this->offset; };
 	inline UINT16 GetStartPoint() { return this->StartPoint; };
-	vector<string> ReadChunkBack(UINT16);
-	vector<string> ReadChunkFront(UINT16);
+	static const inline UINT16 blocksCountX = SCREEN_WIDTH / 32;
+	static const inline UINT16 blocksCountY = SCREEN_HEIGHT / 32;
 private:
 
-	static inline const LPCWSTR imgSrc = L"GameProject/Graphicss/block.png";
-	static inline UINT16 blocksCountX = SCREEN_WIDTH / 32;
-	static inline UINT16 blocksCountY = SCREEN_HEIGHT / 32;
+	static const inline  LPCWSTR imgSrc = L"GameProject/Graphicss/block.png";
 	static inline UINT16 averageFloorLevel = blocksCountY / 2;
 	static inline UINT16 skyLevel = blocksCountY / 5;
 	static vector<Block*> BlockType;
@@ -47,7 +45,6 @@ private:
 	vector<string> chunkTemplate;
 	map<UINT16, UINT16> FloorLevel;
 	UINT16 StartPoint;
-	thread* thr;
 	
 
 
