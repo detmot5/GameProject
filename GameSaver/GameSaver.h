9b9067f28c36e44f5e9505487abf36ccef518a59
Norbert Bielak <norbertbielak@gmail.com>
2020-05-27 16:05:58 +0200

    Working Reading and Writing

--- GameSaver/GameSaver.h
+++ GameSaver/GameSaver.h
@@ -1,14 +1,23 @@
 #pragma once
-#include "../framework.h"
-#include "../Chunks/Chunk.h"
 #include <fstream>
 #include <sstream>
+#include <filesystem>
 #include <vector>
 #include <list>
-
+#include <algorithm>
+#include "../framework.h"
+#include "../Chunks/Chunk.h"
+using namespace std;
 
 namespace GameSaver {
-	list<Chunk*> buffer;
-	
+	void InitSave(wstring saveName);
+	namespace Write {
+		void SaveChunk(Chunk* chunk);	
+	}
+
+	namespace Read {
+		void LoadSave(wstring path);
+		Chunk* GetChunkFromBuffer(UINT16 StartPoint);
+	}
 
 };
