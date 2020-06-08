c4b1a40a0310974d8a61a25c73e2898a58ddd6bd
Norbert Bielak <norbertbielak@gmail.com>
2020-06-08 10:58:52 +0200

    Added files with paths

---
+++ Paths.cpp
@@ -0,0 +1,10 @@
+
+#include "Paths.h"
+
+namespace Path {
+	const LPCWSTR block = L"GameProject/Graphicss/block.png";
+	const LPCWSTR character = L"GameProject/Graphicss/character.png";
+	const LPCWSTR cloud = L"GameProject/Graphicss/Cloud.png";
+	const LPCWSTR savesDirectory = L"GameProject/Saves/";
+
+}
---
+++ Paths.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "framework.h"
+
+// DON'T WRITE "using namespace std"!!
+
+
+namespace Path {
+	extern const LPCWSTR block;
+	extern const LPCWSTR character;
+	extern const LPCWSTR cloud;
+	extern const LPCWSTR savesDirectory;
+
+}
